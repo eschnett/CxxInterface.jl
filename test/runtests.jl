13df54cb060262d2459a99ed2bb374459457e315
Erik Schnetter <schnetter@gmail.com>
2021-12-21 22:15:16 -0500

    Disable tests on unsupported architectures

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,8 +31,19 @@ run(`c++ -fPIC -c AddIntegers.cxx`)
 dlext = Sys.isapple() ? "dylib" : "so"
 run(`c++ -shared -o libAddIntegers.$dlext AddIntegers.o`)
 
+# DO NOT call a C++ compiler manually in your own Julia packages. This
+# works only in very controlled environments such as on CI
+# infrastructure. If you do, your package will be fragile, and will
+# create lots of headaches for your users in the wild. Instead, use
+# [BinaryBuilder](https://binarybuilder.org) and store your build
+# recipes on [Yggdrasil](https://github.com/JuliaPackaging/Yggdrasil).
+
 ################################################################################
 # Call the wrapped function
 # (This fails if the C++ compiler is not compatible with the currently
 # running Julia executable)
-@test AddIntegers.add_int(2, 3) == 5
+
+# Only test cases where the Github CI environment supports this
+if Sys.ARCH ≡ :x86_64
+    @test AddIntegers.add_int(2, 3) == 5
+end
