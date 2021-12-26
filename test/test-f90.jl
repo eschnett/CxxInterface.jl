8f4a4ec274e8b0688d3b83f2edb0473a7989c2b0
Erik Schnetter <schnetter@gmail.com>
2021-12-26 17:50:06 -0500

    CI: Disable Fortran of macOS

--- test/test-f90.jl
+++ test/test-f90.jl
@@ -20,23 +20,26 @@ AddIntegersF90.f90_write_code!()
 ################################################################################
 # Compile Fortran code
 # (This fails if there is no Fortran compiler available)
-using Libdl: dlext
-run(`gfortran -fPIC -c AddIntegersF90.f90`)
-run(`gfortran -shared -o libAddIntegersF90.$dlext AddIntegersF90.o`)
+# macOS does not have a Fortran compiler installed by default
+if !Sys.isapple()
+    using Libdl: dlext
+    run(`gfortran -fPIC -c AddIntegersF90.f90`)
+    run(`gfortran -shared -o libAddIntegersF90.$dlext AddIntegersF90.o`)
 
-# Please, DO NOT call a Fortran compiler manually in your own Julia
-# packages. This works only in very controlled environments such as on
-# CI infrastructure. If you do, your package will be fragile, and will
-# create lots of headaches for your users in the wild. Instead, use
-# [BinaryBuilder](https://binarybuilder.org) and store your build
-# recipes on [Yggdrasil](https://github.com/JuliaPackaging/Yggdrasil).
+    # Please, DO NOT call a Fortran compiler manually in your own Julia
+    # packages. This works only in very controlled environments such as on
+    # CI infrastructure. If you do, your package will be fragile, and will
+    # create lots of headaches for your users in the wild. Instead, use
+    # [BinaryBuilder](https://binarybuilder.org) and store your build
+    # recipes on [Yggdrasil](https://github.com/JuliaPackaging/Yggdrasil).
 
-################################################################################
-# Call the wrapped function
-# (This fails if the Fortran compiler is not compatible with the
-# currently running Julia executable)
+    ################################################################################
+    # Call the wrapped function
+    # (This fails if the Fortran compiler is not compatible with the
+    # currently running Julia executable)
 
-# Only test cases where the Github CI environment supports this (:i686 is not supported)
-if Sys.ARCH ≡ :x86_64
-    @test AddIntegersF90.add_int(2, 3) == 5
+    # Only test cases where the Github CI environment supports this (:i686 is not supported)
+    if Sys.ARCH ≡ :x86_64
+        @test AddIntegersF90.add_int(2, 3) == 5
+    end
 end
