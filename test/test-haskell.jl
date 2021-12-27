2cd863e083306a08d6b398dffe69db1f19785ce1
Erik Schnetter <schnetter@gmail.com>
2021-12-27 10:02:40 -0500

    CI: Disable building Haskell

--- test/test-haskell.jl
+++ test/test-haskell.jl
@@ -41,26 +41,30 @@ AddIntegersHaskell.c_write_code!()
 ################################################################################
 # Compile Haskell code
 # (This fails if there is no Haskell compiler available)
-using Libdl: dlext
-run(`ghc -fPIC -c AddIntegersHaskell.hs`)
-run(`ghc -fPIC -c HaskellRTS.c`)
-run(`ghc -shared -o libAddIntegersHaskell.$dlext AddIntegersHaskell.o HaskellRTS.o -lffi -lHSrts`)
-
-# Please, DO NOT call a Haskell compiler manually in your own Julia
-# packages. This works only in very controlled environments such as on
-# CI infrastructure. If you do, your package will be fragile, and will
-# create lots of headaches for your users in the wild. Instead, use
-# [BinaryBuilder](https://binarybuilder.org) and store your build
-# recipes on [Yggdrasil](https://github.com/JuliaPackaging/Yggdrasil).
-
-################################################################################
-# Call the wrapped function
-# (This fails if the Haskell compiler is not compatible with the
-# currently running Julia executable)
-
-# Only test cases where the Github CI environment supports this (:i686 is not supported)
-if Sys.ARCH ≡ :x86_64
-    AddIntegersHaskell.hs_init()
-    @test AddIntegersHaskell.add_int(2, 3) == 5
-    AddIntegersHaskell.hs_exit()
+if false
+    # This works on macOS with MacPorts, but not on Github Actions
+    using Libdl: dlext
+    v = VersionNumber(split(read(`ghc --version`, String))[end])
+    run(`ghc -fPIC -c AddIntegersHaskell.hs`)
+    run(`ghc -fPIC -c HaskellRTS.c`)
+    run(`ghc -shared -o libAddIntegersHaskell.$dlext AddIntegersHaskell.o HaskellRTS.o -lffi -lHSrts`)
+    
+    # Please, DO NOT call a Haskell compiler manually in your own Julia
+    # packages. This works only in very controlled environments such as on
+    # CI infrastructure. If you do, your package will be fragile, and will
+    # create lots of headaches for your users in the wild. Instead, use
+    # [BinaryBuilder](https://binarybuilder.org) and store your build
+    # recipes on [Yggdrasil](https://github.com/JuliaPackaging/Yggdrasil).
+    
+    ################################################################################
+    # Call the wrapped function
+    # (This fails if the Haskell compiler is not compatible with the
+    # currently running Julia executable)
+    
+    # Only test cases where the Github CI environment supports this (:i686 is not supported)
+    if Sys.ARCH ≡ :x86_64
+        AddIntegersHaskell.hs_init()
+        @test AddIntegersHaskell.add_int(2, 3) == 5
+        AddIntegersHaskell.hs_exit()
+    end
 end
